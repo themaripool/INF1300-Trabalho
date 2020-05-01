8b3dba77a5b9a42e0b0195b18e9c29a233eb2a74
themaripool <marielama@outlook.com.br>
2020-05-01 13:03:00 -0300

    [CHANGE] Tela de home com botoes levando ao grafico e historico de humor

--- inf1300_relax/lib/initial_view.dart
+++ inf1300_relax/lib/initial_view.dart
@@ -1,6 +1,9 @@
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'secondPage.dart';
+import 'thirdPage.dart';
+
 
 
 class MainPage extends StatefulWidget {
@@ -39,33 +42,192 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
+
+    DateTime date = new DateTime.now();
+
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Relax')
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Relax')
+      // ),
       body: Center(
         child: Column(
-
           children: <Widget>[
 
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                 builder: (context) => SecongPage(),
-                ),  
-              );
-            },
-            child: const Text(
-              'Ir nivel 1 de push',
-              style: TextStyle(fontSize: 20)
+            // Card com o título, contendo dia e dia da semana
+            Container(
+              margin: EdgeInsets.only(top: 35, left: 8),
+              width: 300,
+              height: 50,
+              child: 
+              Text("Segunda, ${date.day}/${date.month}",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontStyle: FontStyle.italic,
+                  fontSize: 25
+                ),
+              ),
             ),
-          ),
 
+            // Card com o medidor de humor do dia
+            Card(
+              shadowColor: Colors.black,
+              color: Color.fromRGBO(240, 248, 255, 1),
+              child:Container(
+                  width: 300,
+                  height: 150,
+                  decoration: new BoxDecoration(
+                    color: Color.fromRGBO(240, 248, 255, 1),
+                    borderRadius: new BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Column(
+                    children: <Widget>[
+
+                      Container(
+                        margin: EdgeInsets.only(top: 8),
+                        color: Colors.blueGrey,
+                        width: 250,
+                        height: 50,
+
+
+                      ),
+
+
+
+
+
+
+                    ],
+                  ),
+              )
+            ),
+
+            // Botão temporário para ir pro grafico
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                  builder: (context) => SecongPage(),
+                  ),  
+                );
+              },
+              child: const Text(
+                'Ir nivel 1 de push',
+                style: TextStyle(fontSize: 20)
+              ),
+            ),
+
+            Divider(color: Colors.white,),
+
+            // Row com cards clicaveis para grafico e historico
+            Row(
+              children: <Widget>[
+
+                // Botao da ir para grafico
+
+                GestureDetector(
+
+                  onTap: (){
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => SecongPage(),));
+                  },
+                  
+                  child: Card(
+                    shadowColor: Colors.black,
+                    color: Color.fromRGBO(240, 248, 255, 1),
+                    child:
+                      Container(
+                        width: 150,
+                        height: 90,
+                        decoration: new BoxDecoration(
+                          color: Color.fromRGBO(240, 248, 255, 1),
+                          borderRadius: new BorderRadius.all(Radius.circular(10))
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+
+                            Container(
+                              width: 5,
+                              color: Colors.transparent,
+                            ),
+
+                            Image.asset("assets/iconeGrafico.png",width: 40,height: 40,fit: BoxFit.fill,),
+
+                            Container(
+                              width: 5,
+                              color: Colors.transparent,
+                            ),
+
+                            Expanded(child:
+                              AutoSizeText(
+                                'Gráfico de humor',
+                                style: TextStyle(fontFamily: 'RobotoMono', fontSize: 15),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ) 
+                          ],
+                        ),
+                      ),
+                  )
+                ),
+
+                // Botao da ir pro historico
+                 GestureDetector(
+
+                  onTap: (){
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => ThirdPage(),));
+                  },
+                  
+                  child: Card(
+                    shadowColor: Colors.black,
+                    color: Color.fromRGBO(248, 248, 255, 1),
+                    child:
+                      Container(
+                        width: 150,
+                        height: 90,
+                        decoration: new BoxDecoration(
+                          color: Color.fromRGBO(248, 248, 255, 1),
+                          borderRadius: new BorderRadius.all(Radius.circular(10))
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+
+                            Container(
+                              width: 5,
+                              color: Colors.transparent,
+                            ),
+
+                            Image.asset("assets/iconeHistorico.png", width: 40,height: 40,fit: BoxFit.fill,),
+
+                            Container(
+                              width: 5,
+                              color: Colors.transparent,
+                            ),
+
+                            Expanded(child:
+                              AutoSizeText(
+                                'Histórico de humor',
+                                style: TextStyle(fontFamily: 'RobotoMono', fontSize: 15),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ) 
+                          ],
+                        ),
+                      ),
+                  )
+                )
+
+              ],
+            )
+           
+           
           ],
-
-
 
         )
       ),
