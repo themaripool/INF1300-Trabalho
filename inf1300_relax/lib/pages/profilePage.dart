d2ab79bdf9fcd7c5aad8252a4b2851540089f4f1
themaripool <marielama@outlook.com.br>
2020-06-17 20:01:33 -0300

    [CHANGE] Design tela de perfil

--- inf1300_relax/lib/pages/profilePage.dart
+++ inf1300_relax/lib/pages/profilePage.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'cameraPage.dart';
+import '../colors/customColors.dart';
+import '../pages/cameraPage.dart';
 import '../utility/profileImageUtil.dart';
 
 //class ProfilePage extends StatelessWidget {
@@ -15,33 +15,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  //final File imageFile;
-
-  //ProfilePage(this.imageFile);
   var result;
-  var imageSalva ;
-  Widget _decideImageView(String result) {
-    ProfileImageUtil.getImageFromPreferences().then(
-      (img) {
-        setState(() {
-          imageSalva = img;
-        });
-      }
-    );
-    if (imageSalva == null) {
-      return Text("Nenhuma imagem selecionada");
-    }
-    return Container(
-      height: 200.0,
-      width: 200.0,
-      decoration: new BoxDecoration(
-        shape: BoxShape.circle,
-        image: DecorationImage(
-            image: Image.file(File(imageSalva)).image, fit: BoxFit.cover),
-      ),
-    );
-  }
-
+  var imageSalva;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,24 +25,144 @@ class _ProfilePageState extends State<ProfilePage> {
         backgroundColor: const Color(0xFFFFFFFF).withOpacity(0.0),
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          new GestureDetector(
-              onTap: () async {
-                _navigateAndDisplaySelection(context);
-                //_navigationToCameraPage(context);
-              },
-              child: new Card(
-                child: Text("Take profile picture"),
-              )),
-          _decideImageView(result),
+      body: Stack(
+        children: <Widget>[
+          _viewBackground(),
+          _listViewBuilder(),
         ],
-      )),
+      ),
     );
   }
 
+  Widget _viewBackground() {
+    return Container(
+      height: 200.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [MyColors.purple, MyColors.purple]),
+        // colors: [Colors.indigo.shade300, Colors.indigo.shade500]),
+      ),
+    );
+  }
+
+  Widget _listViewBuilder() {
+    return ListView.builder(
+      itemCount: 2,
+      itemBuilder: _mainListBuilder,
+    );
+  }
+
+  Widget _mainListBuilder(BuildContext context, int index) {
+    if (index == 0) return _buildHeader(context);
+    //if (index == 1) return _previousBody();
+  }
+
+  Widget _previousBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        new GestureDetector(
+            onTap: () async {
+              _navigateAndDisplaySelection(context);
+              //_navigationToCameraPage(context);
+            },
+            child: new Card(
+              child: Text("Escolher foto de perfil"),
+            )),
+        _decideImageView(result),
+      ],
+    );
+  }
+
+  Widget _decideImageView(String result) {
+    ProfileImageUtil.getImageFromPreferences().then((img) {
+      setState(() {
+        imageSalva = img;
+      });
+    });
+    //
+    return Container(
+      height: 5.0,
+      width: 5.0,
+      color: Colors.transparent,
+    );
+  }
+
+  //Widgets Header
+
+  Container _buildHeader(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      height: 240.0,
+      child: Stack(
+        children: <Widget>[
+          _backgroundHeader(),
+          _profileCircularImage()
+        ],
+      ),
+    );
+  }
+
+  Widget _columnOnHeader() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 50.0,
+        ),
+        Text(
+          "Seu nome aqui!",
+          style: Theme.of(context).textTheme.title,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        _previousBody(),
+        SizedBox(
+          height: 16.0,
+        ),
+        Container(
+          height: 40.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _backgroundHeader() {
+    return Container(
+      padding:
+          EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0, bottom: 10.0),
+      child: Material(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        elevation: 5.0,
+        color: Colors.white,
+        child: _columnOnHeader(),
+      ),
+    );
+  }
+
+  Widget _profileCircularImage(){
+    return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Material(
+                elevation: 5.0,
+                shape: CircleBorder(),
+                child: CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: (imageSalva != null)
+                        ? Image.file(File(imageSalva)).image
+                        : AssetImage("assets/placeholder.png")),
+              ),
+            ],
+          );
+  }
+
+  
+//Navegação para tela de camera
   _navigateAndDisplaySelection(BuildContext context) async {
     String imagem = await Navigator.push(
         context, new MaterialPageRoute(builder: (context) => CameraPage()));
